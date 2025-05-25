ac10623d94ea3e64fbad50e9e6135caa425cdeee
Martin Vysny <mavi@vaadin.com>
2025-05-25 14:53:18 +0300

    minor

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(name: "SwiftExperiments", dependencies: []),
-        .executableTarget(name: "Exec", dependencies: [.target(name: "SwiftExperiments")]),
-        .testTarget(name: "Tests", dependencies: [.target(name: "SwiftExperiments")]),
+        .executableTarget(name: "Exec", dependencies: ["SwiftExperiments"]),
+        .testTarget(name: "Tests", dependencies: ["SwiftExperiments"]),
     ]
 )
