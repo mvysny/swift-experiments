cd06623ab2b836b0c780cd89135ceba404d64639
Martin Vysny <mavi@vaadin.com>
2025-05-25 15:42:14 +0300

    more tests

--- Sources/Tests/KoansCollectionsTest.swift
+++ Sources/Tests/KoansCollectionsTest.swift
@@ -20,6 +20,11 @@ final class KoansCollectionsTest: XCTestCase {
         XCTAssertEqual(lidl.customers[1], lidl.findCustomerFrom(city: .Helsinki))
     }
 
+    func testAssociate() {
+        XCTAssertEqual(["kari=kari", "mavi=mavi", "pasi=pasi"], lidl.nameToCustomerMap().map { "\($0.key)=\($0.value.name)" } .sorted())
+        XCTAssertEqual(["kari: Paris", "mavi: Helsinki", "pasi: Paris"], lidl.customerToCityMap().map { "\($0.key.name): \($0.value)" } .sorted())
+    }
+
     /// https://play.kotlinlang.org/koans/Collections/GroupBy/Task.kt
     func testGroupBy() {
         XCTAssertEqual([City.Helsinki : ["mavi"], City.Paris : ["kari", "pasi"]], lidl.groupCustomersByCity().mapValues { $0.map { $0.name } })
