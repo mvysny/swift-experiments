2fa56c155f00476f937b811adc4c64cf46682d37
Martin Vysny <mavi@vaadin.com>
2025-05-25 15:19:31 +0300

    move tests to KotlinKoansTest

--- Sources/SwiftExperiments/kotlinkoans.swift
+++ Sources/SwiftExperiments/kotlinkoans.swift
@@ -428,28 +428,6 @@ extension Shop {
 // todo more: https://play.kotlinlang.org/koans/Collections/Partition/Task.kt
 
 func main() throws {
-    expect(true, MyDate.parse("2024-1-1") < MyDate("2024-1-2"))
-    expect("2024-5-6", "\(MyDate(year: 2024, month: 5, day: 6))")
-    expect(true, checkInRange(date: MyDate("2025-2-2"), first: MyDate("2025-1-4"), last: MyDate("2026-1-1")))
-    expect(MyDate("2024-1-2"), MyDate("2024-1-1").tomorrow())
-    expect(-1, MyDate("2024-1-2").getDays(to: MyDate("2024-1-1")))
-    expect(MyDate("2024-1-2"), MyDate("2024-1-1").plusDays(1))
-    expect(MyDate("2024-1-6"), MyDate("2024-1-1").plusDays(5))
-    expect(MyDate("2024-1-8"), MyDate("2024-1-1").plusDays(7))
-    expect(MyDate("2026-1-1"), MyDate("2024-1-1").plusYears(2))
-
-    var dates: [MyDate] = Array(MyDate("2024-1-1")...MyDate("2024-1-3"))
-    expect("[2024-1-1, 2024-1-2, 2024-1-3]", dates.description)
-    dates = Array(MyDateRange(from: MyDate("2024-1-1"), to: MyDate("2024-1-3")))
-    expect("[2024-1-1, 2024-1-2, 2024-1-3]", dates.description)
-    
-    dates = []
-    iterateOverDateRange(firstDate: MyDate("2024-1-1"), secondDate: MyDate("2024-1-3")) { dates.append($0) }
-    expect("[2024-1-1, 2024-1-2, 2024-1-3]", dates.description)
-    
-    expect(MyDate("2025-1-8"), task1(MyDate("2024-1-1")))
-    expect(MyDate("2026-1-27"), task2(MyDate("2024-1-1")))
-
     expect(["pasi", "mavi", "kari"], lidl.getCustomersSortedByOrders().map { $0.name })
     expect([City.Paris, City.Helsinki], lidl.getCustomerCities())
     expect(["mavi"], lidl.getCustomersFrom(city: .Helsinki).map { $0.name })
--- Sources/Tests/KotlinKoansTest.swift
+++ Sources/Tests/KotlinKoansTest.swift
@@ -64,4 +64,19 @@ final class KotlinKoansTest: XCTestCase {
         XCTAssertEqual(MyDate("2024-1-8"), MyDate("2024-1-1").plusDays(7))
         XCTAssertEqual(MyDate("2026-1-1"), MyDate("2024-1-1").plusYears(2))
     }
+    func testKoanRanges() {
+        var dates: [MyDate] = Array(MyDate("2024-1-1")...MyDate("2024-1-3"))
+        XCTAssertEqual("[2024-1-1, 2024-1-2, 2024-1-3]", dates.description)
+        dates = Array(MyDateRange(from: MyDate("2024-1-1"), to: MyDate("2024-1-3")))
+        XCTAssertEqual("[2024-1-1, 2024-1-2, 2024-1-3]", dates.description)
+    }
+    func testKoanForLoops() {
+        var dates: [MyDate] = []
+        iterateOverDateRange(firstDate: MyDate("2024-1-1"), secondDate: MyDate("2024-1-3")) { dates.append($0) }
+        XCTAssertEqual("[2024-1-1, 2024-1-2, 2024-1-3]", dates.description)
+    }
+    func testKoanOperatorOverloading() {
+        XCTAssertEqual(MyDate("2025-1-8"), task1(MyDate("2024-1-1")))
+        XCTAssertEqual(MyDate("2026-1-27"), task2(MyDate("2024-1-1")))
+    }
 }
